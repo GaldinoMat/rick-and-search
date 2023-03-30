3cc88a2aca0588d7ec233ab838bd8d4698c89aa5
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-03-30 15:09:36 -0300

    test: finish pagination feature, componentize actions to single function

---
+++ __tests__/components/Pagination/PaginationButton/index.spec.tsx
@@ -0,0 +1,23 @@
+import PaginationButton from "@/components/Pagination/PaginationButton";
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+
+describe("Pagination Button component", () => {
+  test("should fire an event click on pagination buttons", () => {
+    const handleClick = jest.fn();
+    render(
+      <PaginationButton
+        handlePageChange={handleClick}
+        pageNumber={1}
+        testid="test-prev-button"
+        url="anyurl.com"
+      />
+    );
+
+    const prevButton = screen.getByTestId("test-prev-button");
+
+    fireEvent.click(prevButton);
+    expect(handleClick).toBeCalledTimes(1);
+    expect(handleClick).toBeCalledWith("anyurl.com");
+  });
+});
--- __tests__/components/Pagination/index.spec.tsx
+++ __tests__/components/Pagination/index.spec.tsx
@@ -1,6 +1,10 @@
 import Pagination, { matchNextPages } from "@/components/Pagination";
+import store, { requestsMiddleware } from "@/store";
+import { ApiLoad } from "@/store/modules/data/types";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import { Provider } from "react-redux";
+import configureStore, { MockStoreEnhanced } from "redux-mock-store";
 
 const mockPageData = (next: string | null, prev: string | null) => {
   return {
@@ -14,15 +18,31 @@ const mockPageData = (next: string | null, prev: string | null) => {
   };
 };
 
+const makeSut = () => {
+  const mockStore = configureStore(requestsMiddleware);
+  return mockStore(store);
+};
+
+const renderPagination = (
+  sutStore: MockStoreEnhanced<unknown, {}>,
+  data: ApiLoad
+) => {
+  render(
+    <Provider store={sutStore}>
+      <Pagination data={data} />
+    </Provider>
+  );
+};
+
 describe("Pagination component", () => {
   test("should render pagination component", () => {
-    render(
-      <Pagination
-        data={mockPageData(
-          "https://rickandmortyapi.com/api/character/?page=3",
-          "https://rickandmortyapi.com/api/character/?page=1"
-        )}
-      />
+    const mockedStore = makeSut();
+    renderPagination(
+      mockedStore,
+      mockPageData(
+        "https://rickandmortyapi.com/api/character/?page=3",
+        "https://rickandmortyapi.com/api/character/?page=1"
+      )
     );
 
     const pagination = screen.getByTestId("test-pagination");
@@ -46,7 +66,7 @@ describe("Pagination component", () => {
     ).toEqual(expectedResult);
   });
 
-  it("should return null for current page when next page is not present", () => {
+  test("should return null for current page when next page is not present", () => {
     const expectedResult = {
       current: null,
       prev: 33,
@@ -60,7 +80,7 @@ describe("Pagination component", () => {
     ).toEqual(expectedResult);
   });
 
-  it("should return null for prev page when previous page is not present", () => {
+  test("should return null for prev page when previous page is not present", () => {
     const expectedResult = {
       current: 1,
       prev: null,
